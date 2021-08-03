acd88c80a2d0665e854e7925c9db6e22f840ee9b
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-03 00:42:25 -0400

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -55201,7 +55201,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49477" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51055" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
